8b87ef1dab2df1a8bad683dec8f5a83309403fcf
Ian B <113297674+adbrian@users.noreply.github.com>
2024-06-14 12:21:59 +0100

    added even more print lines

--- hello.py
+++ hello.py
@@ -1,4 +1,10 @@
 print('hello,world')
 print('hello, again')
-print('ok that\'s enough with the hellos')
+print('ok that\'s enough with the hellos. right?')
+
 # let's now stage the changes using git add
+print('let\'s now add some more lines to check the gutters')
+print('that\'s line 1')
+print('that\'s line 2')
+print('that\'s line 3')
+print('let\'s now check how the gutters work')
