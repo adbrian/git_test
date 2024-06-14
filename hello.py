befec08368b982f5fa4b4c05dd60ed7a4787eea5
Ian B <113297674+adbrian@users.noreply.github.com>
2024-06-14 12:35:23 +0100

    removed and/or modified some lines

--- hello.py
+++ hello.py
@@ -1,10 +1,11 @@
 print('hello,world')
 print('hello, again')
-print('ok that\'s enough with the hellos. right?')
 
 # let's now stage the changes using git add
 print('let\'s now add some more lines to check the gutters')
-print('that\'s line 1')
+print('that\'s line 0')
 print('that\'s line 2')
 print('that\'s line 3')
 print('let\'s now check how the gutters work')
+print('that\'s line 4')
+print('that\'s line 5')
